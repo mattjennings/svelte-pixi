42a3394ca65fec82ca4ecbef0642b7229cfdb70e
mattjennings <me@mattjennings.io>
2022-03-18 20:52:04 -0500

    use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-import adapter from '@sveltejs/adapter-auto'
+import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 import sveld from 'vite-plugin-sveld'
 import path from 'path'
@@ -22,7 +22,7 @@ const config = {
   ],
 
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     package: {
       files: (file) => !file.includes('website'),
     },
