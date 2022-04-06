1f2052a58ee570e29ac9112420dbcdd228584b46
mattjennings <me@mattjennings.io>
2022-04-06 17:34:30 -0500

    update svelte-kit example

--- examples/svelte-kit/src/lib/PixiApp.svelte
+++ examples/svelte-kit/src/lib/PixiApp.svelte
@@ -1,15 +1,9 @@
-<script context="module">
-  import { registerLoaderPlugin } from 'svelte-pixi'
-  import { SpritesheetLoader } from '@pixi/spritesheet'
-  registerLoaderPlugin(SpritesheetLoader)
-</script>
-
 <script>
   import Player from './Player.svelte'
-  import { Pixi, Text, Loader } from 'svelte-pixi'
+  import { Application, Text, Loader } from 'svelte-pixi'
 </script>
 
-<Pixi width={400} height={400} antialias>
+<Application width={400} height={400} antialias>
   <Loader resources={['adventurer/spritesheet.json']}>
     <slot slot="loading">
       <Text text={`Loading...`} style={{ fill: 'white' }} x={0} y={0} />
@@ -18,4 +12,4 @@
     <Text text="move with arrow keys" style={{ fill: 'white' }} x={120} y={0} />
     <Player />
   </Loader>
-</Pixi>
+</Application>
