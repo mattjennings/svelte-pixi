43243a5c40f3296a3f54016acb1bfefa9de5dc11
mattjennings <me@mattjennings.io>
2022-04-21 16:41:00 -0500

    update docs on aliasing

--- examples/vite-custom-pixi-file/vite.config.js
+++ examples/vite-custom-pixi-file/vite.config.js
@@ -4,12 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  optimizeDeps: {
+    exclude: ['svelte-pixi'],
+  },
   resolve: {
     alias: {
-      'pixi.js': 'src/pixi.js',
+      'pixi.js': '/src/pixi.js',
     },
-  },
-  optimizeDeps: {
-    include: ['pixi.js'],
   },
 })
