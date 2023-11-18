2fa879df9d9cf67991cd87031e6882b218102652
Matt Jennings <me@mattjennings.io>
2023-11-17 22:05:15 -0600

    wip: update Text style type

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import type { ITextStyle } from 'pixi.js'
+
   /**
    * @restProps {Container}
    */
@@ -13,7 +15,7 @@
   type T = $$Generic<PIXI.Text>
   type $$Props = Container<T>['$$prop_def'] & {
     text: PIXI.Text['text']
-    style?: PIXI.Text['style']
+    style?: Partial<ITextStyle>
     anchor?: PointLike
     blendMode?: PIXI.Sprite['blendMode']
     pluginName?: PIXI.Sprite['pluginName']
