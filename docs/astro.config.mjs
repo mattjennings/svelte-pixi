b4afde83b0a28e57a1cee44749d588dc61a03fe3
Matt Jennings <me@mattjennings.io>
2023-11-19 21:41:34 -0600

    wip: new docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,13 +3,13 @@ import starlight from '@astrojs/starlight'
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import svelte from '@astrojs/svelte'
-import examples from 'mdsvexamples/vite'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import remarkExamples from './remark-svelte-example.mjs'
-import recast from 'recast'
+import remarkExamples, {
+  EXAMPLE_MODULE_PREFIX,
+} from './remark-svelte-example.mjs'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://astro.build/config
@@ -78,9 +78,9 @@ function examplesVite() {
    * @type {import('vite').Plugin}
    */
   return {
-    name: 'my-plugin', // required, will show up in warnings and errors
+    name: 'astro-examples', // required, will show up in warnings and errors
     resolveId(id) {
-      if (id.includes(`?src=`)) {
+      if (id.includes(EXAMPLE_MODULE_PREFIX) && id.includes('?src')) {
         const [name, srcAttr] = id.split('?src=')
 
         // decode from base64
@@ -94,6 +94,39 @@ function examplesVite() {
       if (virtualFiles.has(id)) {
         return virtualFiles.get(id)
       }
+    },
+    handleHotUpdate(ctx) {
+      const { server } = ctx
+      const modules = []
+
+      const extensions = ['.md', '.mdx']
+
+      // return virtual file modules for parent file
+      if (extensions.some((ext) => ctx.file.endsWith(ext))) {
+        const files = [...virtualFiles.entries()]
+
+        files
+          .map(([id, file]) => ({
+            id,
+            parent: id.split(EXAMPLE_MODULE_PREFIX)[0],
+            updated: file.updated,
+          }))
+          .filter((file) => {
+            return ctx.file.endsWith(file.parent)
+          })
+          .forEach((file) => {
+            const mod = server.moduleGraph.getModuleById(file.id)
+            if (mod) {
+              modules.push(
+                mod,
+                ...mod.clientImportedModules,
+                ...mod.ssrImportedModules,
+              )
+            }
+          })
+      }
+
+      return [...modules, ...ctx.modules]
     },
   }
 }
