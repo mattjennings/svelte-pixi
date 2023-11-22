30b4c1db11355c8f09636845c9575d2221221ccf
Matt Jennings <me@mattjennings.io>
2023-11-22 00:48:00 -0600

    docs

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -4,17 +4,13 @@ const path = require('path')
 function readPackage(pkg, ctx) {
   if (pkg.name) {
     // point svelte-pixi to ./package output for examples
-    if (
-      pkg.name.endsWith('-example') ||
-      pkg.name.startsWith('with-') ||
-      pkg.name === 'docs'
-    ) {
+    if (pkg.name.endsWith('-example') || pkg.name.startsWith('with-')) {
       const dir = path.resolve(__dirname, './package')
       if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir)
       }
 
-      pkg.dependencies['svelte-pixi'] = 'workspace:../../package'
+      pkg.dependencies['svelte-pixi'] = 'workspace:../../dist'
     }
 
     // install peer as dev dependencies
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-import vercel from '@sveltejs/adapter-vercel'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,12 +14,6 @@ const config = {
     }),
     mdsvex(mdsvexConfig),
   ],
-
-  kit: {
-    adapter: vercel({
-      external: ['sveld', 'svelte-preprocess'],
-    }),
-  },
 }
 
 export default config
