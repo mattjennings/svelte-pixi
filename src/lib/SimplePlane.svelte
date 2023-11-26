e52e31199f00f53f869f55e5d4aaf117d90251c6
Matt Jennings <me@mattjennings.io>
2023-11-26 17:31:40 -0600

    svelte 5

--- src/lib/SimplePlane.svelte
+++ src/lib/SimplePlane.svelte
@@ -10,7 +10,7 @@
   import { createApplyProps } from './util/props'
 
   type T = $$Generic<PIXI.SimplePlane>
-  type $$Props = Container<T>['$$prop_def'] & {
+  type Props = Container<T>['$$prop_def'] & {
     texture: PIXI.SimplePlane['texture']
     vertices: PointLike
     geometry?: PIXI.SimplePlane['geometry']
@@ -19,117 +19,45 @@
     drawMode?: PIXI.SimplePlane['drawMode']
   }
 
-  /**
-   * The texture to use
-   *
-   * @type {PIXI.Texture}
-   */
-  export let texture: $$Props['texture']
-
-  /**
-   * The number of vertices in the plane. This is only used on initial mount.
-   * If you need to update the vertices, you will have to update the geometry buffer
-   * from the instance.
-   *
-   * @type {PointLike}
-   */
-  export let vertices: $$Props['vertices']
-
-  /**
-   * Includes vertex positions, face indices, normals, colors, UVs, and
-   * custom attributes within buffers, reducing the cost of passing all this data to the GPU.
-   * Can be shared between multiple Mesh objects.
-   *
-   * @type {PIXI.Geometry}
-   */
-  export let geometry: $$Props['geometry'] = undefined
-
-  /**
-   * Represents the vertex and fragment shaders that processes the geometry and runs on the GPU.
-   * Can be shared between multiple Mesh objects.
-   *
-   * @type {PIXI.Shader|PIXI.MeshMaterial}
-   */
-  export let shader: $$Props['shader'] = undefined
-
-  /**
-   * Represents the WebGL state the Mesh required to render, excludes shader and geometry.
-   * E.g., blend mode, culling, depth testing, direction of rendering triangles, backface, etc.
-   *
-   * @type {PIXI.State}
-   */
-  export let state: $$Props['state'] = undefined
-
-  /**
-   * The way the Mesh should be drawn, can be any of the PIXI.DRAW_MODES constants.
-   *
-   * @type {PIXI.DRAW_MODES}
-   */
-  export let drawMode: $$Props['drawMode'] = undefined
-
-  /**
-   * The PIXI.SimplePlane instance. Can be set or bound to.
-   *
-   * @type {PIXI.SimplePlane}
-   */
-  export let instance: T = new PIXI.SimplePlane(
+  const {
     texture,
-    parsePoint(vertices).x,
-    parsePoint(vertices).y,
-  ) as T
+    vertices,
+    geometry,
+    shader,
+    state,
+    drawMode,
+    children,
+    instance: _instance,
+    ...restProps
+  } = $props<Props>()
 
-  const { applyProp } = createApplyProps<PIXI.SimplePlane>(instance)
+  export const instance = (_instance ??
+    new PIXI.SimplePlane(
+      texture,
+      parsePoint(vertices).x,
+      parsePoint(vertices).y,
+    )) as T
+
   const { invalidate } = getRenderer()
 
-  afterUpdate(() => {
-    invalidate()
+  const { applyProp } = createApplyProps<PIXI.SimplePlane>(instance, {
+    onApply() {
+      invalidate()
+    },
   })
 
-  $: applyProp('geometry', geometry)
-  $: applyProp('shader', shader)
-  $: applyProp('state', state)
-  $: applyProp('drawMode', drawMode)
-  $: applyProp('texture', texture)
+  $effect(() => applyProp('geometry', geometry))
+  $effect(() => applyProp('shader', shader))
+  $effect(() => applyProp('state', state))
+  $effect(() => applyProp('drawMode', drawMode))
+  $effect(() => applyProp('texture', texture))
 
-  $: texture.on('update', () => invalidate())
+  $effect(() => {
+    texture.on('update', invalidate)
+    return () => {
+      texture.off('update', invalidate)
+    }
+  })
 </script>
 
-<Container
-  {...$$restProps}
-  {instance}
-  on:create
-  on:click
-  on:globalmousemove
-  on:globalpointermove
-  on:globaltouchmove
-  on:mousedown
-  on:mousemove
-  on:mouseout
-  on:mouseover
-  on:mouseup
-  on:mouseupoutside
-  on:mouseupoutside
-  on:pointercancel
-  on:pointerdown
-  on:pointermove
-  on:pointerout
-  on:pointerover
-  on:pointertap
-  on:pointerup
-  on:pointerupoutside
-  on:removedFrom
-  on:rightclick
-  on:rightdown
-  on:rightup
-  on:rightupoutside
-  on:tap
-  on:touchcancel
-  on:touchend
-  on:touchendoutside
-  on:touchmove
-  on:touchstart
-  on:added
-  on:removed
->
-  <slot />
-</Container>
+<Container {...restProps} {instance} {children} />
