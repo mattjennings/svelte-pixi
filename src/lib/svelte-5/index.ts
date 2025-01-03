1abdb09715d5f70f21d625a7c3803836504c74b0
Matt Jennings <me@mattjennings.io>
2025-01-02 22:00:50 -0600

    add experimental assets and texture store loaders

--- src/lib/svelte-5/index.ts
+++ src/lib/svelte-5/index.ts
@@ -26,3 +26,4 @@ export {
 } from '../core/context/container'
 export { getRenderer } from '../core/context/renderer'
 export { getTicker, onTick, track, tick } from '../core/context/ticker'
+export { asset, texture } from '../core/util/asset'
